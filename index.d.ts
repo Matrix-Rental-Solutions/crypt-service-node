66816a218b6ce02fe7988869ed172eb892962862
Andrew Sidhu <sid@foyer.ai>
2024-10-08 15:38:00 -0400

    update build

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { CryptService } from "./lib/crypt.service";
-export { CryptAlg } from "./lib/crypt.extras";
-export { CryptOptions } from "./lib/crypt.extras";
--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CryptAlg = exports.CryptService = void 0;
-var crypt_service_1 = require("./lib/crypt.service");
-Object.defineProperty(exports, "CryptService", { enumerable: true, get: function () { return crypt_service_1.CryptService; } });
-var crypt_extras_1 = require("./lib/crypt.extras");
-Object.defineProperty(exports, "CryptAlg", { enumerable: true, get: function () { return crypt_extras_1.CryptAlg; } });
-//# sourceMappingURL=index.js.map
